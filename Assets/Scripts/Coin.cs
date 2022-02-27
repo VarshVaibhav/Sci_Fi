9b52ad06c75b74b4b193ec37ad430cd67b827ad7
Vaibhav Varshney <varshneyvaibhav838@gmail.com>
2022-02-27 16:16:26 +0530

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private AudioClip _coinPickup;
+
+    private void Start()
+    {
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player player = other.GetComponent<Player>();
+                if(player != null)
+                {
+                    player.hasCoin = true;
+                    AudioSource.PlayClipAtPoint(_coinPickup, transform.position, 1f); // main camera position: camera.main.transform.position
+
+                    UIManager uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+                    if(uiManager != null)
+                    {
+                        uiManager.CollectedCoin();
+                    }
+
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Sci_Fi
+ A 3D game project in which we shop and shoot.
